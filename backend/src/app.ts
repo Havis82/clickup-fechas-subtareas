3aac3567b61995a147d2c0fa0872f2c653dcb829
Havis82 <jacor82@hotmail.com>
2025-05-13 13:43:11 +0200

    fix: use dynamic port

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -52,7 +52,7 @@ app.use('/webhook', webhookRoutes);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
