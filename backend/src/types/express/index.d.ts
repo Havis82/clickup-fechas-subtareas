3fad2c4786170f7ca8cf33c098c1bf4408607638
Havis82 <jacor82@hotmail.com>
2025-05-26 11:08:03 +0200

    Update index.d.ts para extender Express. Request

--- backend/src/types/express/index.d.ts
+++ backend/src/types/express/index.d.ts
@@ -1,10 +1,13 @@
-import { User } from '../../config/passport';
-
 declare global {
   namespace Express {
     interface User {
       accessToken: string;
       refreshToken: string;
+    }
+
+    // Esta es la parte nueva para extender Express. Request
+    interface Request {
+      user?: User;
     }
   }
 }
